9df33afd16cbbec7b73c5f38c118cd53e5fe723f
Kuba C <jakub.cieplinski@outlook.com>
2020-06-07 14:07:46 +0200

    decompression of whole lines implemented

--- module1/homework/grayscaleImages/compression.cpp
+++ module1/homework/grayscaleImages/compression.cpp
@@ -22,6 +22,23 @@ std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(const std::array<std:
 
 std::array<std::array<uint8_t, 240>, 160> decompressGrayscale(const std::vector<std::pair<uint8_t, uint8_t>>& image) {
     std::array<std::array<uint8_t, 240>, 160> decompressed{};
+    int sum{};
+    int columnSize = 240;
+
+    for (auto& row : decompressed) {
+        for (auto it = image.begin(); it != image.end();) {
+            int count = std::count_if(image.begin(), image.end(),
+                                      [&sum, columnSize](std::pair<uint8_t, uint8_t> el) { return (sum += el.second) != columnSize; });
+            auto secIt = it;
+            sum = 0;
+            std::vector<uint8_t> rowVector(240);
+            std::advance(secIt, count);
+            std::for_each(it, secIt, [&rowVector](std::pair<uint8_t, uint8_t> el) { for(auto i = el.second; i <= el.second; i++) 
+                                                                                        rowVector.push_back(el.first); });
+            it = secIt;
+            std::copy(rowVector.begin(), rowVector.end(), row.begin());
+        }
+    }
 
     return decompressed;
 }
